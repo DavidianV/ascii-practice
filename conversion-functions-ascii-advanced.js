3f101017d3f7580f563b2b9ffc26835e9a56ab5b
root <root@DESKTOP-ELPBQ96>
2023-06-01 19:41:27 -0400

    easy-squeezy

--- conversion-functions-ascii-advanced.js
+++ conversion-functions-ascii-advanced.js
@@ -10,31 +10,46 @@ Consult documentation on how to utilize the following methods:
 /* Base 2 to base 16 */
 function binaryToHexadecimal(blob) {
   // Your code here
+  let baseTen = parseInt(blob.slice(2), 2);
+  return '0x' + baseTen.toString(16);
 }
 
 /* Base 16 to base 2 */
 function hexadecimalToBinary(blob) {
   // Your code here
+  let baseTen = parseInt(blob.slice(2), 16);
+  return '0b' + baseTen.toString(2);
 }
 
 /* Base 10 to ASCII */
 function decimalToAscii(blob) {
   // Your code here
+  return String.fromCharCode(blob);
 }
 
 /* Base 2 to ASCII */
 function binaryToAscii(blob) {
   // Your code here
+  let baseTen = parseInt(blob.slice(2), 2);
+  return String.fromCharCode(baseTen);
 }
 
 /* Base 16 to ASCII */
 function hexadecimalToAscii(blob) {
   // Your code here
+  let baseTen = parseInt(blob.slice(2), 16);
+  return String.fromCharCode(baseTen);
 }
 
 /* ASCII to base 10 */
 function asciiToDecimal(blob) {
   // Your code here
+  let arr = []
+
+  for (let i = 0; i < blob.length; i++) {
+    arr.push(blob.charCodeAt(i));
+  }
+  return arr
 }
 
 // console.log('Binary to hexadecimal:')
